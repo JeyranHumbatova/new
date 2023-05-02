83bfe3a6151396c7c02361b1ffb0ef06acdfb9bf
JeyranHumbatova <127186057+JeyranHumbatova@users.noreply.github.com>
2023-05-03 00:00:35 +0400

    Add files via upload

---
+++ js-alqoritm/assets/js/main.js
@@ -0,0 +1,29 @@
+function openModal() {
+    const modal = document.querySelector(".window");
+    modal.classList.remove("d-none")
+}
+function closeModal() {
+    const modal = document.querySelector(".window");
+    modal.classList.add("d-none");
+}
+
+
+let index = -1;
+function createStudent() {
+    let arr = [];
+    let nameInp = document.querySelector("input[name='ad']");
+    let surnameInp = document.querySelector("input[name='soyad']");
+
+    arr.push(nameInp.value);
+    arr.push(surnameInp.value);
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML += `<tr><td>${index += 1}</td><td>${arr.shift(1)}</td><td>${arr.pop(1)}</td><td>    <button class="p-0 " ind="" onclick="removeStudent()" ><i class="las la-trash"></i></button></td></tr>`;
+    nameInp.value = '';
+    surnameInp.value = '';
+    closeModal();
+    tbody.removeAttribute("class","d-none");
+}
+function removeStudent() {
+    document.querySelector("tbody").setAttribute("class","d-none")
+
+}
